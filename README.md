57b15d763ef94e23ba6341b0bb7c00513aeed0c9
DaniSanchezKeedio <dsanchez@keedio.com>
2017-01-25 10:22:15 +0100

    Changed FieldCopyInterceptor to allow different names
    - Added test cases to cover the new feature
    - README.md modified to reflect the changes
    - Changed dependency version in pom.xml and added myself as a developer

--- README.md
+++ README.md
@@ -14,9 +14,10 @@ To configure your agent to use this interceptor, first configure all the propert
 	...
 
 	# Fields to copy from the Flume event header to the extraData field
-    a1.sources.r1.interceptors.i1.header.fields.to.copy.1 = header1
-    a1.sources.r1.interceptors.i1.header.fields.to.copy.2 = header2
+    a1.sources.r1.interceptors.i1.header.fields.to.copy.header1 = header1
+    a1.sources.r1.interceptors.i1.header.fields.to.copy.header2 = header2
+    a1.sources.r1.interceptors.i1.header.fields.to.copy.header3 = differentName3
     	
-This will produce an event whose body contains an enriched message in which the extraData field contains the values for headers `header1` and `header2` coming in the input flume event. 
+This will produce an event whose body contains an enriched message in which the extraData field contains the values for headers `header1` and `header2` coming in the input flume event. Also, it will contain the value for header `header3` but with the name `differentName3` in the extraData field. 
 
 If the specified headers do not exist, the extraData field won't be modified.
--- src/test/java/org/keedio/flume/interceptor/enrichment/FieldCopyInterceptorTest.java
+++ src/test/java/org/keedio/flume/interceptor/enrichment/FieldCopyInterceptorTest.java
@@ -10,6 +10,8 @@ import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Luca Rosellini lrosellini@keedio.com on 2/12/15.
@@ -30,6 +32,60 @@ public class FieldCopyInterceptorTest extends EnrichmentInterceptorAbstractTest{
 
             assertTrue(enrichedEventBody.getExtraData().containsKey("filename"));
             assertEquals(enrichedEventBody.getExtraData().get("filename"),"/Users/tmp/hola.txt");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            junit.framework.Assert.fail();
+        }
+    }
+
+    @Test
+    public void testSingleInterceptionDifferentName() {
+        String sourceName = "filename";
+        String targetName = "filenamefinal";
+        String testValue = "/Users/tmp/hola.txt";
+        try {
+            Event event = createEvent("hello");
+            event.getHeaders().put(sourceName, testValue);
+            EnrichmentInterceptor interceptor = createInterceptorDifferentName(sourceName, targetName, "DEFAULT");
+
+            Event intercepted = interceptor.intercept(event);
+
+            EnrichedEventBody enrichedEventBody = EnrichedEventBody.createFromEventBody(intercepted.getBody(), true);
+
+            assertTrue(enrichedEventBody.getExtraData().containsKey(targetName));
+            assertEquals(enrichedEventBody.getExtraData().get(targetName), testValue);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            junit.framework.Assert.fail();
+        }
+    }
+
+    @Test
+    public void testMultipleInterceptionDifferentName() {
+        Map<String, String> headerToExtraData = new HashMap<>();
+        headerToExtraData.put("filename", "filename");  // First with same name
+        headerToExtraData.put("filename", "filenamefinal");  // Second with different name
+        String testValue = "/Users/tmp/hola.txt";  // Same value for both extraData fields
+        try {
+            Event event = createEvent("hello");
+
+            for (Map.Entry<String, String> entry : headerToExtraData.entrySet()) {
+                event.getHeaders().put(entry.getKey(), testValue);
+            }
+
+            EnrichmentInterceptor interceptor = createInterceptorMultipleDifferentName(headerToExtraData, "DEFAULT");
+
+            Event intercepted = interceptor.intercept(event);
+
+            EnrichedEventBody enrichedEventBody = EnrichedEventBody.createFromEventBody(intercepted.getBody(), true);
+
+            // Test all headerToExtraData mappings
+            for (Map.Entry<String, String> entry : headerToExtraData.entrySet()) {
+                assertTrue(enrichedEventBody.getExtraData().containsKey(entry.getValue()));
+                assertEquals(enrichedEventBody.getExtraData().get(entry.getValue()), testValue);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -92,7 +148,35 @@ public class FieldCopyInterceptorTest extends EnrichmentInterceptorAbstractTest{
     protected FieldCopyInterceptor createInterceptor(String filename, String eventType) {
         Context context = new Context();
         context.put(EnrichmentInterceptor.EVENT_TYPE, eventType);
-        context.put("header.fields.to.copy.1","filename");
+        context.put("header.fields.to.copy.filename","filename");
+
+        FieldCopyInterceptor.Builder builder = new FieldCopyInterceptor.Builder();
+        builder.configure(context);
+        FieldCopyInterceptor interceptor = (FieldCopyInterceptor) builder.build();
+        interceptor.initialize();
+        return interceptor;
+    }
+
+    protected FieldCopyInterceptor createInterceptorDifferentName(String sourceName, String targetName, String eventType) {
+        Context context = new Context();
+        context.put(EnrichmentInterceptor.EVENT_TYPE, eventType);
+        context.put("header.fields.to.copy.".concat(sourceName), targetName);
+
+        FieldCopyInterceptor.Builder builder = new FieldCopyInterceptor.Builder();
+        builder.configure(context);
+        FieldCopyInterceptor interceptor = (FieldCopyInterceptor) builder.build();
+        interceptor.initialize();
+        return interceptor;
+    }
+
+
+    protected EnrichmentInterceptor createInterceptorMultipleDifferentName(Map<String, String> headerToExtraData, String eventType) {
+        Context context = new Context();
+        context.put(EnrichmentInterceptor.EVENT_TYPE, eventType);
+
+        for (Map.Entry<String, String> entry : headerToExtraData.entrySet()) {
+            context.put("header.fields.to.copy.".concat(entry.getKey()), entry.getValue());
+        }
 
         FieldCopyInterceptor.Builder builder = new FieldCopyInterceptor.Builder();
         builder.configure(context);
